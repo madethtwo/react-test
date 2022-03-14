18bba7329b5b4909d1aec417b99c171c20661bc4
KAO LEANGSENG <kaoleangseng@KAOs-MacBook-Pro.local>
2022-03-14 11:28:19 +0700

    merge login form with master

--- src/home1.js
+++ src/home1.js
@@ -2,8 +2,11 @@ import React, { Fragment } from "react";
 
 function home1() {
   return (
-    <div>home1 + in master</div>
-  )
+    <Fragment>
+      <div>home1 + in master</div>
+      <div>home1 + in seng branch</div>
+    </Fragment>
+  );
 }
 
 export default home1;
