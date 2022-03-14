89dde707624e4f291f217658c3d4fde8e0824a90
KAO LEANGSENG <kaoleangseng@KAOs-MacBook-Pro.local>
2022-03-14 11:21:26 +0700

    add fragement to cover as container

--- src/home1.js
+++ src/home1.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React, { Fragment } from "react";
 
 function home1() {
   return (
-    <div>home1</div>
-  )
+    <Fragment>
+      <div>home1</div>
+      <div>home1 + in seng branch</div>
+    </Fragment>
+  );
 }
 
-export default home1
+export default home1;
