4534fb95ce462d3b09110bd2ac3d31f2c382e5c0
KAO LEANGSENG <kaoleangseng@KAOs-MacBook-Pro.local>
2022-03-16 16:44:12 +0700

    new nita

--- src/home1.js
+++ src/home1.js
@@ -11,16 +11,16 @@ function home1() {
       <div>home1 + in seng 5</div>
       <div>home1 + in master 6</div>
       <div>home1 + in seng 6</div>
-      <div>home1 + in master 7</div>
-      <div>home1 + in master 8</div>
       <div>home1 + in seng 8</div>
       <div>home1 + in seng 9</div>
       <div>home1 + in nita 9</div>
       <div>home1 + in nita 10</div>
       <div>home1 + in nita 11</div>
-      <div>home1 + in nita 12</div>
+      <div>home1 + in nita 12asdfasdfas</div>
       <div>home1 + in nita 13</div>
       <div>home1 + in nita 14</div>
+      <div>home1 + in nita 15 sdfsdf</div>
+      <div>home1 + in nita 15 sdfsdf</div>
       <div>home1 + in nita 15 sdfsdf</div>
     </Fragment>
   );
